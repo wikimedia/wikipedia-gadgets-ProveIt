09c94ee41def5c7edc450f08b827f8d144a58e81
Sophivorus <schenonef@gmail.com>
2019-11-05 14:54:47 -0800

    Fix minor issue
    Change-Id: I03c684128b83b60a42cb66b837f5ddfe5a89cbba

--- proveit.js
+++ proveit.js
@@ -1289,6 +1289,10 @@ window.ProveIt = {
 					continue;
 				}
 
+				if ( paramName === 'title' ) {
+					
+				}
+
 				// If we reach this point and there's no equal sign, it's an anonymous parameter
 				indexOfEqual = paramString.indexOf( '=' );
 				if ( indexOfEqual === -1 ) {
@@ -1529,7 +1533,7 @@ window.ProveIt = {
 		this.getCitations = function () {
 			var citations = [],
 				wikitext = ProveIt.getWikitext(),
-				citationRegex = new RegExp( '<\s*ref[^/]*/>', 'ig' ),
+				citationRegex = new RegExp( '<ref[^/]*/>', 'ig' ),
 				citationMatch, citationWikitext, citationIndex, citationNameMatch, citationName, citation;
 			while ( ( citationMatch = citationRegex.exec( wikitext ) ) !== null ) {
 				citationWikitext = citationMatch[ 0 ];
